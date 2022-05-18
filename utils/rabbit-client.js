0ea81bd10f41458b2a4e5213f1a19bb2ba5eb385
Cesar Solano <csolanoc@unal.edu.co>
2022-05-18 11:09:43 -0500

    handling error

--- utils/rabbit-client.js
+++ utils/rabbit-client.js
@@ -5,16 +5,21 @@ const queue = process.env.QUEUE || 'hello'
 
 function start() {
   let promise = new Promise(async(resolve, reject) => {
-    let connection = await amqp.connect(`amqp://${process.env.RABBITMQ_HOST}`)
-    let channel = await connection.createChannel()
+    try {
+      let connection = await amqp.connect(`amqp://${process.env.RABBITMQ_HOST}`)
+      let channel = await connection.createChannel()
 
-    channel.assertQueue(queue).then(() => {
-        // console.log('holi', channel)
-        resolve({ connection, channel })
-      }).catch(err => {
-        reject()
-      })
-      // return { connection, channel }
+      channel.assertQueue(queue).then(() => {
+          // console.log('holi', channel)
+          resolve({ connection, channel })
+        }).catch(err => {
+          reject(err)
+        })
+        // return { connection, channel }
+    } catch (error) {
+      reject(error)
+    }
+
   })
   return promise
 }
